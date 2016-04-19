2b93ed4b2be0fa7f1b14a8f89a6255469ce0c2b2
Alejandro Rodriguez <ardzoht@gmail.com>
2016-04-19 10:25:40 -0600

    Opss, fixed some sintaxis bugs

--- api/serializers.py
+++ api/serializers.py
@@ -55,53 +55,53 @@ class PrinterLogSerializer(ModelSerializer):
             if toner_black_counter == "-3":
                 toner_black_counter = "Level OK: over 10 percet to 100 percent"
             elif toner_black_counter == "-100":
-                toner_black_counter == "Low Level: under 10% of toner"
+                toner_black_counter = "Low Level: under 10% of toner"
             
             if toner_cyan_counter == "-3": 
                 toner_cyan_counter = "Level OK: over 10 percet to 100 percent"
             elif toner_cyan_counter == "-100":
-                toner_cyan_counter == "Low Level: under 10% of toner"
+                toner_cyan_counter = "Low Level: under 10% of toner"
             
             if toner_magenta_counter == "-3":
                 toner_magenta_counter = "Level OK: over 10 percet to 100 percent"
             elif toner_magenta_counter == "-100":
-                toner_magenta_counter == "Low Level: under 10% of toner"
+                toner_magenta_counter = "Low Level: under 10% of toner"
             
             if toner_yellow_counter == "-3":
                 toner_yellow_counter = "Level OK: over 10 percet to 100 percent"
             elif toner_yellow_counter == "-100":
-                toner_yellow_counter == "Low Level: under 10% of toner"
+                toner_yellow_counter = "Low Level: under 10% of toner"
 
             if copy_color_counter == "200":
                 copy_color_counter = "Couldn't retrieve this data"
             if copy_black_white_counter == "200":
                 copy_black_white_counter = "Couldn't retrieve this data"
-            if print_color_counter = "200"
+            if print_color_counter == "200":
                 print_color_counter = "Couldn't retrieve this data"
             if total_black_white == "200":
                 total_black_white = "Couldn't retrieve this data"
             if total_color == "200":
                 total_color = "Couldn't retrieve this data"
-            if print_black_white_counter = "200"
+            if print_black_white_counter == "200":
                 print_black_white_counter = "Couldn't retrieve this data"            
             if fax_black_white_counter == "200":
                 fax_black_white_counter = "Couldn't retrieve this data"
             if fax_color_counter == "200":
                 fax_color_counter = "Couldn't retrieve this data"
-            if toner_black_counter = "200"
+            if toner_black_counter == "200":
                 toner_black_counter = "Couldn't retrieve this data"
-            if toner_cyan_counter = "200"
+            if toner_cyan_counter == "200":
                 toner_cyan_counter = "Couldn't retrieve this data"
-            if toner_magenta_counter = "200"
+            if toner_magenta_counter == "200":
                 toner_magenta_counter = "Couldn't retrieve this data"
-            if toner_yellow_counter = "200"
+            if toner_yellow_counter == "200":
                 toner_yellow_counter = "Couldn't retrieve this data"
             
             if total_color == "200":
                 total_color = "0"
             if fax_black_white_counter == "200":
                 fax_black_white_counter = "0"
-            if fax_color_counter = "200"
+            if fax_color_counter == "200":
                 fax_color_counter = "0"
              
 	    message = "The current counters of the MFP (" + print_id + ") are the following: " + "\n\nTotal B&W: " + total_black_white + "\nTotal Color: " + total_color + "\nPrinter B&W Counter: " + print_black_white_counter + "\nPrinter Color Counter: " + print_color_counter + "\nCopier B&W Counter: " + copy_black_white_counter + "\nCopier Color Counter: " + copy_color_counter + "\nFax B&W Counter: " + fax_black_white_counter + "\nFax Color Counter: " + fax_color_counter + "\nBlack Toner Counter: " + toner_black_counter + "\nCyan Toner Counter: " + toner_cyan_counter + "\nMagenta Toner Counter: " + toner_magenta_counter + "\nYellow Toner Counter: " + toner_yellow_counter 
