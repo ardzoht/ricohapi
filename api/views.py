0723819dd77b662633f9b63a56244c937653a43f
Sergio Emir Baidon Carrillo <SergioBaidon@MacBook-Pro-de-Sergio.local>
2016-04-19 09:51:58 -0600

    added dashboard

--- api/views.py
+++ api/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from api.serializers import PrinterSerializer, PrinterLogSerializer, UserSerializer
-from api.models import Printer, User, PrinterLog
+from api.models import Printer, User
+from api.models import PrinterLog as Logs
 from rest_framework.generics import ListCreateAPIView
 from rest_framework import viewsets
+from django.views.generic import ListView
 
 # Create your views here.
 class PrinterLog(viewsets.ModelViewSet):
-	queryset = PrinterLog.objects.all()
+	queryset = Logs.objects.all()
 	serializer_class = PrinterLogSerializer
 
 class Printers(viewsets.ModelViewSet):
@@ -16,3 +18,28 @@ class Printers(viewsets.ModelViewSet):
 class User(viewsets.ModelViewSet):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
+
+class Dashboard(ListView):
+    template_name = "printerlog_list.html"
+
+    def get_queryset(self):
+        if self.request.method == 'GET' and 'q' in self.request.GET:
+            q = self.request.GET['q']
+            option = self.request.GET['dropdown']
+            if q:
+                if option == 'log':
+                    try:
+                        return Logs.objects.filter(log_id=q)
+                    except ValueError:
+                        return Logs.objects.filter(log_id=None)
+                elif option == 'date':
+                    q=q.split('-')
+                    return Logs.objects.filter(timestamp__year=q[0], timestamp__month=q[1], timestamp__day=q[2])
+                elif option == 'printer':
+                    return Logs.objects.filter(fk_printer=q)
+            else:
+                    return Logs.objects.order_by('timestamp')
+        else:
+            return Logs.objects.order_by('timestamp')
+
+
--- ricoh/urls.py
+++ ricoh/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework import routers
-from api.views import PrinterLog, Printers, User
+from api.views import PrinterLog, Printers, User, Dashboard
 
 router = routers.DefaultRouter()
 router.register('printers', Printers)
@@ -11,5 +11,6 @@ urlpatterns = patterns(
     # url(r'^$', 'ricoh.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^dashboard/', Dashboard.as_view(), name='dashboard'),
     url(r'^api/', include(router.urls))
 )
